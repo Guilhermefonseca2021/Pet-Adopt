d4cfcd26e4cb3ec3d8ff39b480fd22a1fa0ae0ec
guilherme fonseca <gsbloogs194@gmail.com>
2023-11-07 19:52:08 -0300

    setup

--- src/pages/MovieHome/index.tsx
+++ src/pages/MovieHome/index.tsx
@@ -4,15 +4,12 @@ import { useEffect, useState } from "react";
 export default function index() {
   const [topMovies, setTopMovies] = useState([]);
 
-  const getTopRatedMovies = async(url: ) => {
+  const getTopRatedMovies = async(url: any) => {
     fetch('https://')
       .then(response => response.json())
       .then(data => setTopMovies(data))
   }, [])
 
-  useEffect(() => {
-    
-  }, [])
 
   return (
     <div>
